70373c05eaed3ca290bab12a84f3c4ffef6711c6
Sumit Patel <patelsumitunjha@gmail.com>
2020-01-22 17:13:26 +0530

    New Page ConfigurationPortal 1

---
+++ ConfigPortal/Pages/ConfigurationPortal_1.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConfigPortal.Pages
+{
+    public class ConfigurationPortal_1Model : PageModel
+    {
+        public string ServerName { get; set; }
+        public string DatabaseName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConString { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+            ServerName = Request.Form[nameof(ServerName)];
+            DatabaseName = Request.Form[nameof(DatabaseName)];
+            UserName = Request.Form[nameof(UserName)];
+            Password = Request.Form[nameof(Password)];
+            ConString = Request.Form[nameof(ConString)];
+
+            string ConnectionString = "Server=" + ServerName + ";Database=" + DatabaseName + ";Uid=" + UserName + ";psw=" + Password + ";";
+            ConString = "Connection String :: " + ConnectionString;
+
+            Directory.CreateDirectory(Environment.CurrentDirectory + "/OutputFiles");
+            string path = Environment.CurrentDirectory + "/OutputFiles/" + "ConnectionString.txt";
+
+            System.IO.File.WriteAllText(path, ConnectionString);
+
+            
+
+        }
+
+    }
+}
