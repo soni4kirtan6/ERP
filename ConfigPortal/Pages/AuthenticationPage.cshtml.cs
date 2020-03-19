cd029ccbec22e1e3cd30be3358a0e354dc83ab24
Sumit Patel <patelsumitunjha@gmail.com>
2020-03-19 17:12:09 +0530

    Authentication Back end

--- ConfigPortal/Pages/AuthenticationPage.cshtml.cs
+++ ConfigPortal/Pages/AuthenticationPage.cshtml.cs
@@ -32,7 +32,7 @@ namespace ConfigPortal.Pages
             }
 
 
-            string path = Environment.CurrentDirectory + "/OutputFiles/" + "authentication.json";
+            string path = Environment.CurrentDirectory + "/OutputFiles/" + "Authentication.json";
             JObject authentication_json = JObject.Parse(System.IO.File.ReadAllText(path));
 
             string Role = Request.Form["role_name"];
@@ -43,53 +43,58 @@ namespace ConfigPortal.Pages
                 string TableName = table.Name.ToString();
 
 
-                string[] crud = { "-","-","-","-"};
-                string c = Request.Form["c-"+TableName];
+                string crud = "";
+                string c = Request.Form["c-" + TableName];
                 string r = Request.Form["r-" + TableName];
                 string u = Request.Form["u-" + TableName];
                 string d = Request.Form["d-" + TableName];
-                if (c.Equals("c"))crud[0] = "C";
-                if (r.Equals("r"))crud[1] = "R";
-                if (u.Equals("u"))crud[2] = "U";
-                if (d.Equals("d"))crud[3] = "D";
+                if (c != null && c.Equals("c")) crud += "C"; else crud += "-";
+                if (r != null && r.Equals("r")) crud += "R"; else crud += "-";
+                if (u != null && u.Equals("u")) crud += "U"; else crud += "-";
+                if (d != null && d.Equals("d")) crud += "D"; else crud += "-";
 
-                string[] readArray;
-                string[] writeArray;
-
-               
-                readArray= Request.Form["readvalues-"+TableName];
-                writeArray= Request.Form["writevalues-" + TableName];
-
-                JArray not_readArray = new JArray();
-                JArray not_writeArray = new JArray();
-
-                foreach (var col in table.Value)
+                if (!crud.Equals("----"))
                 {
-                    if (!readArray.Contains(col.ToString()))
+                    string[] readArray;
+                    string[] writeArray;
+
+
+                    readArray = Request.Form["readvalues-" + TableName];
+                    writeArray = Request.Form["writevalues-" + TableName];
+
+                    JArray not_readArray = new JArray();
+                    JArray not_writeArray = new JArray();
+
+                    foreach (var col in table.Value)
                     {
-                        not_readArray.Add(col.ToString());
+                        if (!readArray.Contains(col.ToString()))
+                        {
+                            not_readArray.Add(col.ToString());
+                        }
+                        if (!writeArray.Contains(col.ToString()))
+                        {
+                            not_writeArray.Add(col.ToString());
+                        }
                     }
-                    if (!writeArray.Contains(col.ToString()))
-                    {
-                        not_readArray.Add(col.ToString());
-                    }
+                    table_sub_obj.Add("CRUD", crud.ToString());
+
+                    //add inverted columns
+                    if (crud.Contains("R"))
+                        table_sub_obj.Add("!ReadColumns", not_readArray);
+                    if (crud.Contains("C") || crud.Contains("U") || crud.Contains("D"))
+                        table_sub_obj.Add("!WriteColumns", not_writeArray);
+                    //add table only if one of crud is selected
+                    table_obj.Add(TableName, table_sub_obj);
                 }
-                table_sub_obj.Add("CRUD",crud.ToString());
 
-                //add inverted columns
-                table_sub_obj.Add("!ReadColumns", not_readArray);
-                table_sub_obj.Add("!WriteColumns",not_writeArray);
-
-                //add table only if one of crud is selected
-                table_obj.Add(TableName,table_sub_obj);
             }
-                authentication_json.Add(Role, table_obj);
+            authentication_json.Add(Role, table_obj);
             //Think about how to initialize the json first time configuring
 
 
 
             Directory.CreateDirectory(Environment.CurrentDirectory + "/OutputFiles");
-            string path1 = Environment.CurrentDirectory + "/OutputFiles/" + "authentication.json";
+            string path1 = Environment.CurrentDirectory + "/OutputFiles/" + "Authentication.json";
 
 
             System.IO.File.WriteAllText(path1, authentication_json.ToString());
